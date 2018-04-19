739a4d2b0db8f9377378250f11e6fad1521e909f
Edward Seidy Izawa <seidyizawa@hotmail.com>
2018-04-19 17:48:14 -0300

    FAZER GAMMA E LIMIAR

--- PraticoProcessamento/PraticoProcessamento.py
+++ PraticoProcessamento/PraticoProcessamento.py
@@ -14,15 +14,18 @@ def limiarizacao(img1,lim):
     
 def gamma(img1,gamma):
     img2 = copy.copy(img1)
-    img2 = ajuste_gamma(img1, gamma)
+    for i in range(0, img1.shape[0]-1):
+        for j in range(0, img1.shape[1]-1):
+            gam = gamma ** img1[i, j]
+            img2[i,j] = gam
     cv2.imwrite('ativ2.png',img2)
     return img2;
 
-def ajuste_gamma(image, gamma):
-   iGamma = 1.0 / gamma
-   tabela = np.array([((i / 255.0) ** iGamma) * 255
-      for i in np.arange(0, 256)]).astype("uint8")
-   return cv2.LUT(image, tabela);
+#def ajuste_gamma(image, gamma):
+#   iGamma = 1.0 / gamma
+#   tabela = np.array([((i / 255.0) ** iGamma) * 255
+#      for i in np.arange(0, 256)]).astype("uint8")
+#   return cv2.LUT(image, tabela);
 
 def negativo(img1):
     img2 = copy.copy(img1)
@@ -44,8 +47,8 @@ def media(img1,mask):
     mascara = np.ones((mask,mask),np.float32)/(mask*mask)
     for i in range(mask, img1.shape[0]-1):
         for j in range(mask, img1.shape[1]-1):
-            for y in range(y-mask/2,y+mask/2):
-                for x in range(x-mask/2,x+mask/2):
+            for y in range(i-mask/2,i+mask/2):
+                for x in range(j-mask/2,j+mask/2):
                    masc.extend(img1[y,x])
             img2[i,j] = np.mean(masc)
             masc = []
@@ -58,8 +61,8 @@ def mediana(img1,mask):
     mascara = np.ones((mask,mask),np.float32)/(mask*mask)
     for i in range(mask, img1.shape[0]-1):
         for j in range(mask, img1.shape[1]-1):
-            for y in range(y-mask/2,y+mask/2):
-                for x in range(x-mask/2,x+mask/2):
+            for y in range(i-mask/2,i+mask/2):
+                for x in range(j-mask/2,j+mask/2):
                    masc.extend(img1[y,x])
             masc.sort()
             img2[i,j] = masc[mask*mask/2]
@@ -73,8 +76,8 @@ def min(img1,mask):
     mascara = np.ones((mask,mask),np.float32)/(mask*mask)
     for i in range(mask, img1.shape[0]-1):
         for j in range(mask, img1.shape[1]-1):
-            for y in range(y-mask/2,y+mask/2):
-                for x in range(x-mask/2,x+mask/2):
+            for y in range(i-mask/2,i+mask/2):
+                for x in range(j-mask/2,j+mask/2):
                    masc.extend(img1[y,x])
             masc.sort()
             img2[i,j] = masc[0]
@@ -88,8 +91,8 @@ def max(img1,mask):
     mascara = np.ones((mask,mask),np.float32)/(mask*mask)
     for i in range(mask, img1.shape[0]-1):
         for j in range(mask, img1.shape[1]-1):
-            for y in range(y-mask/2,y+mask/2):
-                for x in range(x-mask/2,x+mask/2):
+            for y in range(i-mask/2,i+mask/2):
+                for x in range(j-mask/2,j+mask/2):
                    masc.extend(img1[y,x])
             masc.sort()
             masc.reverse()
@@ -106,13 +109,13 @@ def show(img1,img2):
         cv2.destroyAllWindows()
     return
 
-img = cv2.imread('img01-a.png',cv2.IMREAD_COLOR)
+img = cv2.imread('img02-a.jpg',cv2.IMREAD_COLOR)
 lim = 50
-gamma = 0.5
+gamma = 2.0
 inthini = 60
 inthfim = 200
 intvfim = 200
-infvini = 60
+intvini = 60
 mask = 3
 #show(img,limiarizacao(img,lim))
 show(img,gamma)
